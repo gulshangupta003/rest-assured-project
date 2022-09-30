67858f466d47e94e8e71491ee2e162455a9975c3
gulshangupta1 <gulshang@testvagrant.com>
2022-09-30 12:09:46 +0530

    adding first test

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,6 +1,8 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class TestOne {
     @Test
     public void shouldGetAllUsers() {
@@ -9,6 +11,11 @@ public class TestOne {
         // Assert
 
         given()
+                .when()
+                    .get("https://reqres.in/api/users")
+                .then()
+                    .statusCode(200)
+                    .log().body();
 
         Assert.assertEquals(1, 1);
     }

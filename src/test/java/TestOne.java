02b55b15852972ea53cb6430307ff0f9262d5fe6
gulshangupta1 <gulshang@testvagrant.com>
2022-10-01 17:53:23 +0530

    Added second test which creates new user

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,11 +13,34 @@ public class TestOne {
 
         given()
                 .when()
-                    .get("https://reqres.in/api/users")
+                    .get("https://gorest.co.in/public/v2/users")
                 .then()
                     .statusCode(200)
                     .log().body();
 
         Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void shouldCreateUser() {
+        // Arrange
+        // Act
+        // Assert
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer 457a143dbd57bef631313eb0e02b0777a8d268c3ae52c43ea34aa4581d7e0ed5")
+                .body("{\n" +
+                        "    \"name\": \"Tenali Ramakrishna\",\n" +
+                        "    \"gender\": \"male\",\n" +
+                        "    \"email\": \"tenalir4@gmail.com\",\n" +
+                        "    \"status\": \"active\"\n" +
+                        "}")
+                .when()
+                    .post("https://gorest.co.in/public/v2/users")
+                .then()
+                .log().body()
+                .statusCode(201);
     }
 }
